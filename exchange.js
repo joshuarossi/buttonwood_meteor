0bafdb5429f859e7cabdf059566d95aef3742a2f
josh <joshua.rossi@btcshares.com>
2013-05-25 11:58:27 -0400

    Testing externally defined functions

--- exchange.js
+++ exchange.js
@@ -68,7 +68,7 @@ if (Meteor.isClient) {
     'click input.add': function() { click_input_add("ask"); }
   });
   Template.bid_list.events({
-    'click input.add': function() { click_input_add("bid"); }
+    'click input.add': function() { click_input_add("bid"); foo(); }
   });
   Template.ask_list.events({
     'click input.remove': function(){ Asks.remove(this._id); }
