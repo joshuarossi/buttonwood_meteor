394bc5a53a9453f9dfbeebd70e99f3872a356599
Maximo Joshua Rossi <maximojoshuarossi@gmail.com>
2013-05-25 11:02:31 -0400

    Geolocation test, using watchPosition and tying that to a session variable

--- exchange.js
+++ exchange.js
@@ -1,6 +1,30 @@
 Bids = new Meteor.Collection("bids");
 Asks = new Meteor.Collection("asks");
 
+function get_location() {
+    if (navigator.geolocation) {
+        var timeoutVal = 10 * 1000 * 1000;
+        navigator.geolocation.watchPosition(
+            setPosition(),
+            displayError(),
+            { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+        );
+    }
+    else {
+        alert("Geolocation is not supported by this browser");
+    }
+}
+function setPosition(position) {
+    Session.set("location", [position.coords.latitude, position.coords.longitude]);
+}
+function displayError() {
+    var errors = {
+        1: 'Permission denied',
+        2: 'Position unavailable',
+        3: 'Request timeout'
+    };
+    alert("Error: " + errors[error.code]);
+}
 function click_input_add(kind) {
   var user = Meteor.user();
   if (user === null) {
