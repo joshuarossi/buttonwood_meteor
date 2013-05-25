7e8d204185de26110012cc9f76d26f7a46b5de4d
josh <joshua.rossi@btcshares.com>
2013-05-25 13:58:35 -0400

    Testing externally defined functions

--- exchange.js
+++ exchange.js
@@ -1,37 +1,23 @@
 Bids = new Meteor.Collection("bids");
 Asks = new Meteor.Collection("asks");
 
-
-
-function displayPosition(position) {
-    alert("Latitude: " + Position.coords.latitude + ", Longitude: " + Position.coords.longitude);
+function getLocation(){
+    navigator.geolocation.getCurrentPosition(setLocation, noLocation);
 }
-
-setPosition = function (position) {
-    Session.set("location", {"latitude": Position.coords.latitude, "longitude": Position.coords.longitude});
-};
-displayError = function () {
-    var errors = {
-        1: 'Permission denied',
-        2: 'Position unavailable',
-        3: 'Request timeout'
-    };
-    alert("Error: " + errors[error.code]);
-};
-
-get_location = function () {
-    if (navigator.geolocation) {
-        var timeoutVal = 10 * 1000 * 1000;
-        navigator.geolocation.watchPosition(
-            displayPosition(Position),
-            displayError(),
-            { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
-        );
-    }
-    else {
-        alert("Geolocation is not supported by this browser");
-    }
-};
+function setLocation(position){
+    Session.set("location", {"latitude": position.coords.latitude, "longitude": position.coords.longitude});
+    displayLocation(position);
+}
+function displayLocation(position)
+{
+var lat = position.coords.latitude;
+var long = position.coords.longitude;
+alert('Found location: ' + lat + ', ' + long);
+}
+function noLocation()
+{
+alert('Could not find location');
+}
 
 function click_input_add(kind) {
   var user = Meteor.user();
--- location.js
+++ location.js
@@ -1,28 +1,28 @@
-get_location = function () {
-    if (navigator.geolocation) {
-        var timeoutVal = 10 * 1000 * 1000;
-        navigator.geolocation.watchPosition(
-            displayPosition(),
-            displayError(),
-            { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
-        );
-    }
-    else {
-        alert("Geolocation is not supported by this browser");
-    }
-};
-function displayPosition(position) {
-    alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
-}
-
-setPosition = function (position) {
-    Session.set("location", {"latitude": position.coords.latitude, "longitude": position.coords.longitude});
-};
-displayError = function () {
-    var errors = {
-        1: 'Permission denied',
-        2: 'Position unavailable',
-        3: 'Request timeout'
-    };
-    alert("Error: " + errors[error.code]);
-};
+//get_location = function () {
+//    if (navigator.geolocation) {
+//        var timeoutVal = 10 * 1000 * 1000;
+//        navigator.geolocation.watchPosition(
+//            displayPosition(),
+//            displayError(),
+//            { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+//        );
+//    }
+//    else {
+//        alert("Geolocation is not supported by this browser");
+//    }
+//};
+//function displayPosition(position) {
+//    alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+//}
+//
+//setPosition = function (position) {
+//    Session.set("location", {"latitude": position.coords.latitude, "longitude": position.coords.longitude});
+//};
+//displayError = function () {
+//    var errors = {
+//        1: 'Permission denied',
+//        2: 'Position unavailable',
+//        3: 'Request timeout'
+//    };
+//    alert("Error: " + errors[error.code]);
+//};
