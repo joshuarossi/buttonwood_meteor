06fdfbfaf6ef8331649f4dc98b361d3020557d94
josh <joshua.rossi@btcshares.com>
2013-05-25 13:38:46 -0400

    Testing externally defined functions

--- exchange.js
+++ exchange.js
@@ -1,6 +1,34 @@
 Bids = new Meteor.Collection("bids");
 Asks = new Meteor.Collection("asks");
 
+get_location = function () {
+    if (navigator.geolocation) {
+        var timeoutVal = 10 * 1000 * 1000;
+        navigator.geolocation.watchPosition(
+            displayPosition(position),
+            displayError(),
+            { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
+        );
+    }
+    else {
+        alert("Geolocation is not supported by this browser");
+    }
+};
+function displayPosition(position) {
+    alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+}
+
+setPosition = function (position) {
+    Session.set("location", {"latitude": position.coords.latitude, "longitude": position.coords.longitude});
+};
+displayError = function () {
+    var errors = {
+        1: 'Permission denied',
+        2: 'Position unavailable',
+        3: 'Request timeout'
+    };
+    alert("Error: " + errors[error.code]);
+};
 
 function click_input_add(kind) {
   var user = Meteor.user();
@@ -28,7 +56,7 @@ function click_input_add(kind) {
 }
 
 if (Meteor.isClient) {
-
+  get_location();
 
   Template.bid_list.bids = function() {
     return Bids.find({}, {sort: {price: -1}});
